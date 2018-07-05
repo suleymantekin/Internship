846dba33a61b9b815caeb12e154656cc565ab555
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-05 13:04:57 +0300

    Answers for exercises of Introduction chapter

---
+++ Phase-1/C# Beginner Tutorials/01 - Introduction to C# Beginner Course.cs
@@ -0,0 +1,18 @@
+/*
+    Author: Süleyman Tekin
+    Date  : 05.07.2018
+ */
+
+
+// 4.1 Getting Started with Exercises
+// Use Console.WriteLine(...) to write "Hello Universe" to the Console window.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello Universe");
+	}
+}
