2810a99b87da21f7c8e17d2cdab3ff263e332e29
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-08 17:07:11 +0300

    Print everything

--- Phase-1/Introduction to C#/Mod1_Self_Assesment_Lab/Mod1_Self_Assesment_Lab/Program.cs
+++ Phase-1/Introduction to C#/Mod1_Self_Assesment_Lab/Mod1_Self_Assesment_Lab/Program.cs
@@ -10,15 +10,32 @@ namespace Mod1_Self_Assesment_Lab
             // Student Information
             Console.WriteLine("Enter student name");
             string studentFirstName = Console.ReadLine();
+
             Console.WriteLine("Enter student last name");
             string studentLastName = Console.ReadLine();
-            DateTime studentBirthDate;
-            string studentAdressLine1 = "";
-            string studentAdressLine2 = "";
-            string studentCity = "";
-            string studentState = "";
-            string studentPostal = "";
-            string studentCountry = "";
+
+            Console.WriteLine("Enter student birthdate");
+            DateTime studentBirthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter student address line 1");
+            string studentAdressLine1 = Console.ReadLine();
+
+            Console.WriteLine("Enter student address line 2");
+            string studentAdressLine2 = Console.ReadLine();
+
+            Console.WriteLine("Enter student city");
+            string studentCity = Console.ReadLine();
+
+            Console.WriteLine("Enter student state");
+            string studentState = Console.ReadLine();
+
+            Console.WriteLine("Enter student postal");
+            string studentPostal = Console.ReadLine();
+
+            Console.WriteLine("Enter student country");
+            string studentCountry = Console.ReadLine();
+
+
 
             // Teacher Information
             string teacherFirstName = "Paul";
@@ -49,10 +66,49 @@ namespace Mod1_Self_Assesment_Lab
             string teacher = "Dan Johnson";
 
 
+            // Print Student Information
+            Console.WriteLine("\n\n\n------Print Student Information------");
+            Console.WriteLine(studentFirstName);
+            Console.WriteLine(studentLastName);
+            Console.WriteLine($"Born on {studentBirthDate}");
+            Console.WriteLine(studentAdressLine1 + " " + studentAdressLine2);
+            Console.WriteLine(studentCity);
+            Console.WriteLine(studentState);
+            Console.WriteLine(studentPostal);
+            Console.WriteLine(studentCountry + "\n\n\n");
 
+            // Print Teacher Information
+            Console.WriteLine("------Print Teacher Information------");
             Console.WriteLine(teacherFirstName);
             Console.WriteLine(teacherLastName);
             Console.WriteLine($"Born on {teacherBirthDate}");
+            Console.WriteLine(teacherAdressLine1 + " " + teacherAdressLine2);
+            Console.WriteLine(teacherCity);
+            Console.WriteLine(teacherState);
+            Console.WriteLine(teacherPostal);
+            Console.WriteLine(teacherCountry + "\n\n\n");
+
+
+            //Print UProgram Information
+            Console.WriteLine("------Print UProgram Information------");
+            Console.WriteLine(programName);
+            Console.WriteLine(departmentHead);
+            Console.WriteLine(string.Join(" ", degrees) + "\n\n\n");
+
+
+            // Print Degree Information
+            Console.WriteLine("------Degree Course Information------");
+            Console.WriteLine(degreeName);
+            Console.WriteLine(creditsRequired + "\n\n\n");
+
+
+            // Print Course Information
+            Console.WriteLine("------Print Course Information------");
+            Console.WriteLine(courseName);
+            Console.WriteLine(credits);
+            Console.WriteLine(durationInWeeks);
+            Console.WriteLine(teacher + "\n\n\n");
+
         }
     }
 }
