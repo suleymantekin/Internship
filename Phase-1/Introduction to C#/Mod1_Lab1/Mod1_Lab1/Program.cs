00d748b983df23fd5bc5e0c26a12f5ced038fbfa
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-08 16:30:15 +0300

    Introduction to C# Module 1 assignment solutions

---
+++ Phase-1/Introduction to C#/Mod1_Lab1/Mod1_Lab1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mod1_Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string firstName = "";
+            string lastName = "";
+            int age = 0;
+            string street = "";
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+
+            firstName = "Tom";
+            lastName = "Thumb";
+            age = 18;
+            street = "123 Fourth Street";
+            city = "Anytown";
+            country = "MyCountry";
+            birthDate = new DateTime(1932, 6, 1);
+
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            Console.WriteLine("{0} years old.", age);
+
+
+            Console.WriteLine(street + ", " + city + ", " + country);
+
+            // use string interpolation
+            Console.WriteLine($"Born on {birthDate}");
+        }
+    }
+}
---
+++ Phase-1/Introduction to C#/Mod1_Self_Assesment_Lab/Mod1_Self_Assesment_Lab/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Mod1_Self_Assesment_Lab
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            // Student Information
+            Console.WriteLine("Enter student name");
+            string studentFirstName = Console.ReadLine();
+            Console.WriteLine("Enter student last name");
+            string studentLastName = Console.ReadLine();
+            DateTime studentBirthDate;
+            string studentAdressLine1 = "";
+            string studentAdressLine2 = "";
+            string studentCity = "";
+            string studentState = "";
+            string studentPostal = "";
+            string studentCountry = "";
+
+            // Teacher Information
+            string teacherFirstName = "Paul";
+            string teacherLastName = "Cole";
+            DateTime teacherBirthDate = new DateTime(1995, 3, 8);
+            string teacherAdressLine1 = "12341 Black Street";
+            string teacherAdressLine2 = "Green Avenue";
+            string teacherCity = "Northdown";
+            string teacherState = "Seattle";
+            string teacherPostal = "SCV234";
+            string teacherCountry = "USA";
+
+            //UProgram Information
+            string programName = "Engineering";
+            string departmentHead = "Metin Zontul";
+            string[] degrees = { "Software Engineering", "Civil Engineering" };
+
+
+            //Degree Information
+
+            string degreeName = "Software Engineering";
+            int creditsRequired = 120;
+
+            // Course Information
+            string courseName = "Data Structures";
+            int credits = 4;
+            int durationInWeeks = 14;
+            string teacher = "Dan Johnson";
+
+
+
+            Console.WriteLine(teacherFirstName);
+            Console.WriteLine(teacherLastName);
+            Console.WriteLine($"Born on {teacherBirthDate}");
+        }
+    }
+}
