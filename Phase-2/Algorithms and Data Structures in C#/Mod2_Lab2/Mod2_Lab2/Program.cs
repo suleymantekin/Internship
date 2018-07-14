e224188288ceab7af8e0069a13e2e0fc9d2ee419
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-14 10:25:46 +0300

    Algorithms and Data Structures in C# Module 2 Lab 2 Enumeration solution

---
+++ Phase-2/Algorithms and Data Structures in C#/Mod2_Lab2/Mod2_Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mod2_Lab2
+{
+    class MainClass
+    {
+        enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
+        public static void Main(string[] args)
+        {
+            int x = (int)Days.Sun;
+            int y = (int)Days.Fri;
+            Console.WriteLine("Sun = {0}", x);
+            Console.WriteLine("Fri = {0}", y);
+            Console.WriteLine(Days.Thu);
+        }
+    }
+}
