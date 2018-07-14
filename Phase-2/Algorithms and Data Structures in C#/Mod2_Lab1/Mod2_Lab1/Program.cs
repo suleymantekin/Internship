83644109071f754e9a139ea04fdd2cea1857cbac
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-14 10:23:45 +0300

    Algorithms and Data Structures in C# Module 2 Lab 1 solution

---
+++ Phase-2/Algorithms and Data Structures in C#/Mod2_Lab1/Mod2_Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Mod2_Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Chevy";
+            myCar.Model = "Silverado";
+            myCar.Color = "Gray";
+            Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} that is {myCar.Color} in color.");
+            Console.WriteLine();
+            myCar.Accelerate();
+        }
+    }
+
+    struct Car
+    {
+        public string Make;
+        public string Model;
+        public string Color;
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Vroom");
+        }
+    }
+}
