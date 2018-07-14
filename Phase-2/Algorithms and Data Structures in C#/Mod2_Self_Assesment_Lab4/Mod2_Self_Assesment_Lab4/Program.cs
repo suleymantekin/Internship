1e3f786a2a2c10f6dcc7ae26b05ca3a98aded0c9
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-14 11:50:33 +0300

    Algorithms and Data Structures in C# Module 2 Self Assessment Lab 4 : K-th Smallest solution

---
+++ Phase-2/Algorithms and Data Structures in C#/Mod2_Self_Assesment_Lab4/Mod2_Self_Assesment_Lab4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Mod2_Self_Assesment_Lab4
+{
+    class MainClass
+    {
+        
+        public static void Main(string[] args)
+        {
+            //K - th Smallest
+            //Description
+            //Find k-th smallest element in an integer array.You may assume the k - th smallest value exists in the array.
+
+            //Example
+
+            //Input : [2, 1, 4]
+            //        and k = 3
+            //Output: 4
+            //_____________________________
+
+            //Input : [7, 2, 1, 6, 1]
+            //        and k = 3
+            //Output: 6
+            int[] array = new int[] { 7, 2, 1, 6, 1 };
+            Console.WriteLine(Ksmallest(array, 3));
+        }
+
+        public static int Ksmallest(int[] a, int k)
+        {
+            Array.Sort(a);
+            var list = new System.Collections.ArrayList();
+
+            foreach (int num in a)
+            {
+                if (!list.Contains(num))
+                    list.Add(num);
+            }
+
+            return (int)list[k - 1];
+        }
+    }
+}
