2e8e58df09ffc7ae3d7a60669f2bfaf3784e5fe7
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-15 11:59:00 +0300

    Algorithms and Data Structures in C# Module 3 Lab 1 solution

---
+++ Phase-2/Algorithms and Data Structures in C#/Mod3_Lab1/Mod3_Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace Mod3_Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //ArrayList myArrList = new ArrayList();
+
+            //myArrList.Add("First Item");
+            //myArrList.Add(3);
+            //myArrList.Add(4.5);
+
+            //foreach (object obj in myArrList)
+            //{
+            //    Console.WriteLine(obj.ToString());
+            //}
+
+
+            ArrayList myArrList = new ArrayList();
+            myArrList.Add("First Item");
+            myArrList.Add("Third Item");
+            myArrList.Add("Second Item");
+
+            myArrList.Sort();
+            int itemIndex = myArrList.IndexOf("Third Item");
+
+            foreach (object obj in myArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Third Item is at index {itemIndex}.");
+        }
+    }
+}
