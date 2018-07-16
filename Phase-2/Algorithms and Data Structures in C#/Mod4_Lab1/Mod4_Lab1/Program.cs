e659e46a72f59ead272f2d15c062a1538b936ef8
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-16 11:41:49 +0300

    Algorithms and Data Structures in C# Module 4 Lab 1 solution

---
+++ Phase-2/Algorithms and Data Structures in C#/Mod4_Lab1/Mod4_Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mod4_Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+
+            var Ali = new Student("Ali", "Er", DateTime.Now, "1st Ave", " ", "Moncton", "NB", "E1C 4W3", "Canada");
+            var Dan = new Student("Dan", "Johnson", DateTime.Now, "1st Ave", " ", "Moncton", "NB", "E1C 4W3", "Canada");
+            var Paul = new Student("Paul", "Cole", DateTime.Now, "1st Ave", " ", "Moncton", "NB", "E1C 4W3", "Canada");
+
+            students.Add(Ali);
+            students.Add(Dan);
+            students.Add(Paul);
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+
+            students.Remove(Ali);
+            Console.WriteLine($"The number of students is {students.Count}");
+        }
+    }
+}
