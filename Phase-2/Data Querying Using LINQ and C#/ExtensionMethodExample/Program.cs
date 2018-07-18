09e3248d2c75f8dffbba3ea5adf1cbf8a00bc3d0
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-18 12:07:23 +0300

    Add extension method example
    Data Querying Using LINQ and C# Module 1 ExtensionMethodExample

---
+++ Phase-2/Data Querying Using LINQ and C#/ExtensionMethodExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExtensionMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var pi = 3.14;
+            // //var pif = string.Format("${0}", pi);
+            // var pif = pi.FormatWith("${0}");
+            // System.Console.WriteLine(pif);
+
+            var name = "Timothy";
+            var nameFormatted = name.FormatWith("Hello, {0}");
+            System.Console.WriteLine(nameFormatted);
+
+        }
+    }
+
+    static class FormatExtension
+    {
+        public static string FormatWith(this object caller, string template)
+        {
+            if (string.IsNullOrEmpty(template) || !template.Contains("{0}"))
+            {
+                throw new ArgumentException("Please provide a valid format template.");
+            }
+
+            var result = string.Format(template, caller);
+            return result;
+        }
+    }
+}
