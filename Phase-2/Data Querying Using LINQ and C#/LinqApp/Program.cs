73702cb6d567825658260987bdab4e36594ab164
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-19 11:49:00 +0300

    Use OfType operator

--- Phase-2/Data Querying Using LINQ and C#/LinqApp/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/LinqApp/Program.cs
@@ -8,16 +8,28 @@ namespace LinqApp
     {
         static void Main(string[] args)
         {
-            var record = DataLoader.Load(@"./");
-            var femaleTop10 = record
-                .Where(r => r.Gender == Gender.Female && r.Rank <= 10);
-            var maleTop10 = from r in record
-                            where r.Gender == Gender.Male && r.Rank <= 10
-                            select r;
-            foreach (var r in femaleTop10)
-                System.Console.WriteLine(r);
-            foreach (var r in maleTop10)
-                System.Console.WriteLine(r);
+            // The Where Operator
+            // var record = DataLoader.Load(@"./");
+            // var femaleTop10 = record
+            //     .Where(r => r.Gender == Gender.Female && r.Rank <= 10);
+            // var maleTop10 = from r in record
+            //                 where r.Gender == Gender.Male && r.Rank <= 10
+            //                 select r;
+            // foreach (var r in femaleTop10)
+            //     System.Console.WriteLine(r);
+            // foreach (var r in maleTop10)
+            //     System.Console.WriteLine(r);
+
+            //----------------------------------------------------------------------------
+
+            // The OfType Operator
+
+            var objects = new Object[] { 1, 10L, 1.1, 1.1f, "Hello", 2, 3 };
+            var result = objects.OfType<int>();
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
