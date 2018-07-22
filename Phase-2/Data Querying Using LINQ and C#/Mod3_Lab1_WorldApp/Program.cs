92721977bac2980d76c9c49d75073f4b80d6a2f6
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-22 22:31:07 +0300

    Print whether a given city is in the database

--- Phase-2/Data Querying Using LINQ and C#/Mod3_Lab1_WorldApp/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/Mod3_Lab1_WorldApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Mod3_Lab1_WorldApp.Models;
 using System.Linq;
 
@@ -43,10 +44,32 @@ namespace Mod3_Lab1_WorldApp
             // Is there any continent data stored in the database?
             // Is there any city with a population that is greater than 10 million ?
 
-            var r1 = dbContext.Continent.Any();
-            var r2 = dbContext.City.Any(c => c.Population > 10000000);
-            Console.WriteLine(r1);
-            Console.WriteLine(r2);
+            // var r1 = dbContext.Continent.Any();
+            // var r2 = dbContext.City.Any(c => c.Population > 10000000);
+            // Console.WriteLine(r1);
+            // Console.WriteLine(r2);
+
+            //--------------------------------------------------------------------------------------------------------------------------
+            // The Contains Operator
+            // Find out whether a given city is in the database
+
+            var seattle = new City { Name = "Seattle", District = "Washington" };
+            var result = dbContext.City.ToList()
+                .Contains(seattle, new CityEqualityComparer());
+            System.Console.WriteLine(result);
+        }
+    }
+
+    class CityEqualityComparer : IEqualityComparer<City>
+    {
+        public bool Equals(City x, City y)
+        {
+            return x.Name == y.Name && x.District == y.District;
+        }
+
+        public int GetHashCode(City obj)
+        {
+            return obj.GetHashCode();
         }
     }
 }
