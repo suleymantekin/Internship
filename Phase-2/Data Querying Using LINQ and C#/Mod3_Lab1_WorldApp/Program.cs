6dca2a88835a2263cffc68c56c6400ab24a0ec6f
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-23 14:54:35 +0300

    Use Union, Intersect and Except of World database

--- Phase-2/Data Querying Using LINQ and C#/Mod3_Lab1_WorldApp/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/Mod3_Lab1_WorldApp/Program.cs
@@ -85,14 +85,49 @@ namespace Mod3_Lab1_WorldApp
             // The SelectMany Operator
             // Could you list the name of counties in North America and South America?
 
-            var names = dbContext.Continent
-                  .Include(nameof(Continent.Country))
-                  .Where(c => c.Name == "North America" || c.Name == "South America")
-                  .SelectMany(c => c.Country).Select(c => c.Name);
+            // var names = dbContext.Continent
+            //       .Include(nameof(Continent.Country))
+            //       .Where(c => c.Name == "North America" || c.Name == "South America")
+            //       .SelectMany(c => c.Country).Select(c => c.Name);
 
-            foreach (var n in names)
+            // foreach (var n in names)
+            // {
+            //     System.Console.WriteLine(n);
+            // }
+
+            //--------------------------------------------------------------------------------------------------------------------------
+            // The Intersect, Except, and Union Operator
+            // Countries that either have a large surface area or have a large population.
+            // Countries that have both large area and large population.
+            // Countries that have a large area but not a large population.
+
+            var bigArea = dbContext.Country
+                .OrderByDescending(c => c.SurfaceArea).Take(10);
+            var bigPopulation = dbContext.Country
+                .OrderByDescending(c => c.Population).Take(10);
+
+            var r1 = bigArea.Union(bigPopulation);
+            var r2 = bigArea.Intersect(bigPopulation);
+            var r3 = bigArea.Except(bigPopulation);
+
+            Console.WriteLine("[Big Area or Big Population]");
+            foreach (var r in r1)
             {
-                System.Console.WriteLine(n);
+                Console.WriteLine(r.Name);
+            }
+
+            Console.WriteLine("========================");
+            Console.WriteLine("[Big Area and Big Population]");
+            foreach (var r in r2)
+            {
+                Console.WriteLine(r.Name);
+            }
+
+            Console.WriteLine("========================");
+            Console.WriteLine("[Big Area but Not Big Population]");
+            foreach (var r in r3)
+            {
+                Console.WriteLine(r.Name);
             }
         }
     }
