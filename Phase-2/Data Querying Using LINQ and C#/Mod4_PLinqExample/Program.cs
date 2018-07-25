e65ae1e65728e4024e92b609ed2e7580a6f527d3
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-25 15:40:36 +0300

    Order preservation example

--- Phase-2/Data Querying Using LINQ and C#/Mod4_PLinqExample/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/Mod4_PLinqExample/Program.cs
@@ -7,11 +7,24 @@ namespace Mod4_PLinqExample
     {
         static void Main(string[] args)
         {
-            int[] source = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var query1 = source
+            // int[] source = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // var query1 = source
+            //     .Where(n => n % 2 == 1).Select(n => -n);
+            // var query2 = source.AsParallel()
+            //     .Where(n => n % 2 == 1).Select(n => -n);
+
+            // Console.WriteLine(string.Join(", ", query1));
+            // Console.WriteLine(string.Join(", ", query2));
+
+
+            //-----------------------------------------------------------------------
+            // Order Preservation in PLINQ
+
+            int[] source = Enumerable.Range(0, 20).ToArray();
+            var query1 = source.AsParallel().AsOrdered()
                 .Where(n => n % 2 == 1).Select(n => -n);
-            var query2 = source.AsParallel()
-                .Where(n => n % 2 == 1).Select(n => -n);
+            var query2 = source.AsParallel().AsOrdered()
+                .Where(n => n % 2 == 1).AsUnordered().Select(n => -n);
 
             Console.WriteLine(string.Join(", ", query1));
             Console.WriteLine(string.Join(", ", query2));
