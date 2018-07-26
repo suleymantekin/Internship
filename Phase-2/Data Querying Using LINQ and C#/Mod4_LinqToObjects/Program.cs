b3cfc8d2858cc60ee5e9e68fd4a70f093d523935
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-26 16:11:46 +0300

    Split string to use LINQ operator

---
+++ Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Mod4_LinqToObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameString = "Tim,Ella,Tom,Gary,Gerry,Andrew,Marwa,Bre'Ana,Li";
+            var names = nameString.Split(",");
+            var lookup = names.ToLookup(n => n[0], n => n);
+            Console.WriteLine(string.Join(",", lookup['G']));
+        }
+    }
+}
