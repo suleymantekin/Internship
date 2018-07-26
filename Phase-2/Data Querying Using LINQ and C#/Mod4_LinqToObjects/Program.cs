7808392e27008183c7397c8e070cad7df05e6f49
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-26 16:22:01 +0300

    Use Linq to read data from a text file

--- Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToObjects/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToObjects/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Mod4_LinqToObjects
 {
@@ -26,6 +28,39 @@ namespace Mod4_LinqToObjects
                 if (item.Char < 'A' || item.Char > 'Z') continue;
                 Console.WriteLine($"{item.Char}: {item.Count}");
             }
+
+            // LINQ to Text File
+
+            var lines = File.ReadAllLines(@"./");
+            var names2 = lines.Select(l => l.Split(",")[1]);
+            var result2 = from n in names
+                          group n by n[0] into g
+                          orderby g.Key
+                          select $"{g.Key},{g.Count()}";
+            File.WriteAllLines(@"./count.csv", result2);
+
+            var reader = new StreamReader(File.OpenRead(@"./male.csv"));
+            var writer = new StreamWriter(File.OpenWrite(@"./count.csv"));
+            var dict = new Dictionary<char, int>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var name = line.Split(",")[1];
+                if (!dict.ContainsKey(name[0]))
+                {
+                    dict[name[0]] = 0;
+                }
+                dict[name[0]]++;
+            }
+
+            var kvps = dict.OrderBy(kvp => kvp.Key);
+            foreach (var kvp in kvps)
+            {
+                writer.WriteLine($"{kvp.Key},{kvp.Value}");
+            }
+
+            writer.Flush();
+            writer.Close();
         }
     }
 }
