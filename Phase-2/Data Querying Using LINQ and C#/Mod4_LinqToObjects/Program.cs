715c3d0243f0bcb09f204be9235192e9799d73d2
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-26 16:14:52 +0300

    Use string as character collection to use LINQ operators

--- Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToObjects/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToObjects/Program.cs
@@ -7,10 +7,25 @@ namespace Mod4_LinqToObjects
     {
         static void Main(string[] args)
         {
+            // Splitting a String
+
             var nameString = "Tim,Ella,Tom,Gary,Gerry,Andrew,Marwa,Bre'Ana,Li";
             var names = nameString.Split(",");
             var lookup = names.ToLookup(n => n[0], n => n);
             Console.WriteLine(string.Join(",", lookup['G']));
+
+            // String as Character Collection
+
+            var result = from c in nameString
+                         group c by Char.ToUpper(c) into g
+                         orderby g.Key
+                         select new { Char = g.Key, Count = g.Count() };
+
+            foreach (var item in result)
+            {
+                if (item.Char < 'A' || item.Char > 'Z') continue;
+                Console.WriteLine($"{item.Char}: {item.Count}");
+            }
         }
     }
 }
