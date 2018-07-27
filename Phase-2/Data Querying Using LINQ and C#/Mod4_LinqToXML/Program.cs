428dd725a4b8de17517d38c53dcbc4a15bb3eb71
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-27 18:37:20 +0300

    Query world.xml to get the continent names and population of North America

--- Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToXML/Program.cs
+++ Phase-2/Data Querying Using LINQ and C#/Mod4_LinqToXML/Program.cs
@@ -14,41 +14,60 @@ namespace Mod4_LinqToXML
         {
             // Read data
             var dbContext = new WorldContext();
-            var continents = dbContext.Continent.Include(c => c.Country)
-                    .ThenInclude(country => country.City);
+            // var continents = dbContext.Continent.Include(c => c.Country)
+            //         .ThenInclude(country => country.City);
 
-            // Construct XML
-            var rootElement = new XElement("world");
-            foreach (var continent in continents)
+            // // Construct XML
+            // var rootElement = new XElement("world");
+            // foreach (var continent in continents)
+            // {
+            //     var continentElement = new XElement("continent");
+            //     continentElement.Add(new XAttribute("name", continent.Name));
+            //     foreach (var country in continent.Country)
+            //     {
+            //         var countryElement = new XElement("country");
+            //         countryElement.Add(new XAttribute("code", country.Code));
+            //         countryElement.Add(new XAttribute("name", country.Name));
+            //         countryElement.Add(new XAttribute("area", country.SurfaceArea));
+            //         countryElement.Add(new XAttribute("population", country.Population));
+            //         foreach (var city in country.City)
+            //         {
+            //             var cityElement = new XElement("city");
+            //             cityElement.Add(new XAttribute("id", city.Id));
+            //             cityElement.Add(new XAttribute("name", city.Name));
+            //             cityElement.Add(new XAttribute("population", city.Population));
+            //             countryElement.Add(cityElement);
+            //         }
+            //         continentElement.Add(countryElement);
+            //     }
+            //     rootElement.Add(continentElement);
+            // }
+
+            // // Persist in-memory XML data to XML file
+            // var writer = XmlWriter.Create(@"./world.xml");
+            // rootElement.WriteTo(writer);
+            // writer.Flush();
+            // writer.Close();
+            // System.Console.WriteLine("DONE!");
+
+            //---------------------------------------------------------------------------------
+
+            var rootElement = XElement.Load(@"./world.xml");
+
+            // Query 1:
+            var continents = rootElement.Elements()
+                .Select(e => e.Attribute("name").Value);
+            foreach (var item in continents)
             {
-                var continentElement = new XElement("continent");
-                continentElement.Add(new XAttribute("name", continent.Name));
-                foreach (var country in continent.Country)
-                {
-                    var countryElement = new XElement("country");
-                    countryElement.Add(new XAttribute("code", country.Code));
-                    countryElement.Add(new XAttribute("name", country.Name));
-                    countryElement.Add(new XAttribute("area", country.SurfaceArea));
-                    countryElement.Add(new XAttribute("population", country.Population));
-                    foreach (var city in country.City)
-                    {
-                        var cityElement = new XElement("city");
-                        cityElement.Add(new XAttribute("id", city.Id));
-                        cityElement.Add(new XAttribute("name", city.Name));
-                        cityElement.Add(new XAttribute("population", city.Population));
-                        countryElement.Add(cityElement);
-                    }
-                    continentElement.Add(countryElement);
-                }
-                rootElement.Add(continentElement);
+                System.Console.WriteLine(item);
             }
 
-            // Persist in-memory XML data to XML file
-            var writer = XmlWriter.Create(@"./world.xml");
-            rootElement.WriteTo(writer);
-            writer.Flush();
-            writer.Close();
-            System.Console.WriteLine("DONE!");
+            // Query 2:
+            var northAmericaPop = rootElement.Elements()
+                .Single(e => e.Attribute("name").Value == "North America")
+                .Descendants("country")
+                .Sum(e => int.Parse(e.Attribute("population").Value));
+            System.Console.WriteLine(northAmericaPop);
         }
     }
 }
