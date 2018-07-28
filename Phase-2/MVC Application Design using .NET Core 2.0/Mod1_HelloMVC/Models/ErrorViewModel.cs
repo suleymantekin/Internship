c9e4ec9c2243903b5b50450e234c808e8eb67c4e
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-28 23:09:01 +0300

    First commit

---
+++ Phase-2/MVC Application Design using .NET Core 2.0/Mod1_HelloMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mod1_HelloMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
