f19baf7deac1392f04cc6258e306c85fbee09463
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-29 22:57:36 +0300

    Create a set of Model, View, and Controller

---
+++ Phase-2/MVC Application Design using .NET Core 2.0/Mod1_HelloMVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Mod1_HelloMVC.Models;
+
+namespace Mod1_HelloMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ShowAll()
+        {
+            ViewData["Heading"] = "All Products";
+            var products = new List<Product>();
+            products.Add(new Product { ID = 101, Name = "Apple", Price = 1.1 });
+            products.Add(new Product { ID = 202, Name = "Bike", Price = 2.2 });
+            products.Add(new Product { ID = 303, Name = "Calculator", Price = 3.3 });
+            return View(products);
+        }
+    }
+}
---
+++ Phase-2/MVC Application Design using .NET Core 2.0/Mod1_HelloMVC/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace Mod1_HelloMVC.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
