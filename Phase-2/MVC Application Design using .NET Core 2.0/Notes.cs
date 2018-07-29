faae1d92938bebf61770c66439f80623ff51eab8
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-29 21:53:44 +0300

    Rapid Application Development notes

--- Phase-2/MVC Application Design using .NET Core 2.0/Notes.cs
+++ Phase-2/MVC Application Design using .NET Core 2.0/Notes.cs
@@ -6,3 +6,16 @@
 // Because the controller can disappear completely from a well-designed, model-enhanced 
 // pattern, the name of MVC should change to MV. But the name of MV is too short, ambiguous, and 
 // doesn't highlight the enhanced view-model, hence the name Model-View-ViewModel (MVVM)was born.
+
+//--------------------------------------------------------------------------------------------------
+
+// For example, in a button event handler method, we can validate the text in text boxes, 
+// extract the value from the selected item in a dropdown list, 
+// examine if checkboxes are checked, and then talk to the local file system, 
+// database or web services to persist the data. We call this methodology rapid-application-development (RAD).
+
+// RAD is just fine for small applications, but when applications grow larger and larger, 
+// the relationships between the UI instances start getting messy. 
+// Especially when multiple developers are working on code for the same group of UIs, 
+// trying to understand the logic from other developers starts to get harder and harder. 
+// The cost of maintaining the code, resolving code conflict, and fixing bugs go up.
