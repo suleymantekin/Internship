6ff795a89e82848cac42e659d4a2f1b2e241e567
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-30 11:11:20 +0300

    Connect to db and print actor names

--- Phase-2/MVC Application Design using .NET Core 2.0/Mod2_Lab1_Sakila/Program.cs
+++ Phase-2/MVC Application Design using .NET Core 2.0/Mod2_Lab1_Sakila/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using Mod2_Lab1_Sakila.Models;
+using System.Linq;
 namespace Mod2_Lab1_Sakila
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dbContext = new sakilaContext();
+            var actors = dbContext.Actor.ToList();
+            foreach (var a in actors)
+            {
+                System.Console.WriteLine($"ID:{a.ActorId} Name:{a.FirstName} {a.LastName}");
+            }
         }
     }
 }
