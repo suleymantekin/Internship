532445188be19c3a7e8b61dfc31ec3097e72be78
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-30 11:07:51 +0300

    Add  Entity Framework to the project

---
+++ Phase-2/MVC Application Design using .NET Core 2.0/Mod2_Lab1_Sakila/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod2_Lab1_Sakila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
